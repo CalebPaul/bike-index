6afe24d730eeff4266a1077bda08c6acd89052c1
Caleb Paul and Stephen Emery <stephen@>
2016-10-13 12:03:25 -0700

    Add scripts to images.

--- js/bike.js
+++ js/bike.js
@@ -8,7 +8,7 @@ function Bike() {
 
 Bike.prototype.getMaps = function(){
   var newScript = document.createElement("script");
-  newScript.src = "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap";
+  newScript.src = "https://maps.googleapis.com/maps/api/js?&key=" + apiKey + "&callback=initMap";
 
   var placeholder = document.getElementById("placeholder");
   document.body.insertBefore(newScript, placeholder);
@@ -17,19 +17,21 @@ Bike.prototype.getMaps = function(){
 Bike.prototype.getBike = function(make, color) {
     $.get('https://bikeindex.org/api/v2/bikes_search?page=1&per_page=10&colors='+color+'&manufacturer='+make).then(function(response) {
     console.log(response);
+    var i = 0;
     for (var bike of response.bikes) {
       console.log(bike);
       var title = bike.title;
       var thumblink;
       if (bike.thumb === null) {
-        thumblink = '<img class="img" src="/img/icon.png" alt="pic of bike" />';
+        thumblink = '<img id="bike'+i+'" class="img" src="/img/icon.png" alt="pic of bike" />';
       } else {
-        thumblink ='<img class="img" src="'+bike.thumb+'" alt="pic of bike" />';
+        thumblink ='<img id="bike'+i+'" class="img" src="'+bike.thumb+'" alt="pic of bike" />';
       }
-
+      i++;
       $('.showBikes').append('<div class="bike col-sm-4 col-sm-offset-6">' +
       '<h4>'+title+'</h4>'+
-      thumblink +
+       thumblink +
+       '<script> $("[id^=bike]").click(function(event) {event.preventDefault(); alert("'+bike.stolen_location+'"); });</script>' +
       '</div>');
     }
   }).fail(function(error) {
