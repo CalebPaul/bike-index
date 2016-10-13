3b92f4524f016616d87a527547d39014bd801938
Caleb Paul and Stephen Emery <caleb@>
2016-10-13 09:45:39 -0700

    Add initial map to index.html.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,7 +1,18 @@
 var Bike =  require('./../js/bike.js').bikeModule;
 
+// function showMap(){
+//   var map;
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+// };
+
 $(document).ready(function() {
 var newBike = new Bike();
+
+// newBike.getMaps();
+// showMap();
 
   $('#button').click(function(event) {
     event.preventDefault();
